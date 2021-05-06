93fade792a268307a66379230a7bda27a519c0bf
yoonsangmin <dbstkdalsa@naver.com>
2021-05-06 21:55:28 +0900

    Paper, File, Tag

---
+++ VR Horror Game Project/Assets/GrabSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabSound : MonoBehaviour
+{
+    public AudioClip audioClip;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.gameObject.AddComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource.spatialBlend = 1.0f;
+        audioSource.volume = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Grab()
+    {
+        audioSource.Play();
+    }
+}
