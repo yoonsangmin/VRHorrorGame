2d7cb0e7c5709f4189c56479bf2c031fcc6c3aa6
yoonsangmin <dbstkdalsa@naver.com>
2022-04-21 18:41:25 +0900

    Change directories

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonStartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ButtonToTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void ToGameOverScene()
+    {
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    public void ToGameClearScene()
+    {
+        SceneManager.LoadScene("GameClearScene");
+    }
+}
